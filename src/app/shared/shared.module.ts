b15400f47dfd31bb5e9629453857cd2876184ea3
Luis Ricardo Teixeira Fernandes <lrtf.developer@gmail.com>
2019-12-04 12:49:04 -0300

    subindo projeto dashboard  angular 8 e angular material

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MatDividerModule, MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule, MatNavList, MatListModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatListModule,
+    RouterModule
+  ],
+  exports: [
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
